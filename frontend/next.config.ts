2f48f5ac4834f7bd97da488c7047ff829f0b3855
DavidCambra18 <cambrafontan.david@gmail.com>
2026-02-05 12:27:57 +0100

    ajustes del .env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,24 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: false,
+  },
   async rewrites() {
     return [
       {
-        // Esto permite que el Frontend use rutas relativas '/api/...'
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:3000/api/:path*', 
+        destination: 'http://127.0.0.1:4000/api/:path*', 
       },
     ];
+  },
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_OTHER_VAR: process.env.NEXT_PUBLIC_OTHER_VAR,
   },
 };
 
