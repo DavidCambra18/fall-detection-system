93ab67401bc93e48a23e346b1584d4376369f873
Victor-Picallo <victor.p9646@gmail.com>
2026-02-06 22:36:33 +0100

    Sidebar del dashboard

--- frontend/src/app/shared/components/sidebar/sidebar.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common'; // Importante para el *ngIf
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.html'
 })
 export class SidebarComponent {
-  private authService = inject(AuthService);
+  // Inyectamos el servicio como público para usarlo en el HTML
+  public authService = inject(AuthService);
   private router = inject(Router);
 
   onLogout() {
