6b284224eb2ac57be0e52f546e4ea4369ea1363c
Victor-Picallo <victor.p9646@gmail.com>
2026-02-07 01:42:28 +0100

    Adapto Login y Sidebar a todos los roles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,37 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { DashboardComponent } from './features/admin-dashboard/dashboard.component'; 
+import { CuidadorDashboardComponent } from './features/cuidador-dashboard/cuidador-dashboard.component'; 
 import { UsersComponent } from './features/users/users.component';
-import { AlertsComponent } from './features/alerts/alerts.component'; // Importamos el nuevo componente
+import { AlertsComponent } from './features/alerts/alerts.component';
 import { authGuard } from './core/guards/auth-guard';
 import { DevicesComponent } from './features/devices/devices.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  
+  // RUTA PARA ADMIN (Rol 1)
   { 
-    path: 'dashboard', 
+    path: 'admin-dashboard', 
     component: DashboardComponent, 
     canActivate: [authGuard] 
   },
+
+  // RUTA PARA CUIDADOR (Rol 2)
+  { 
+    path: 'cuidador-dashboard', 
+    component: CuidadorDashboardComponent, 
+    canActivate: [authGuard] 
+  },
+
+/*   // RUTA PARA USUARIO (Rol 3)
+  { 
+    path: 'usuario-dashboard', 
+    component: UsuarioDashboardComponent, 
+    canActivate: [authGuard] 
+  }, */
+
+  // Gestión y Seguimiento
   { 
     path: 'users', 
     component: UsersComponent, 
@@ -28,6 +47,8 @@ export const routes: Routes = [
     component: AlertsComponent, 
     canActivate: [authGuard] 
   },
+
+  // Redirecciones
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
